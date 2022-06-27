2f4739fb3d167a3cc249f6ed03d5c332a0bdd6a6
Shawn Radebe <radebeshawnn@gmail.com>
2022-06-28 01:05:19 +0200

    fixing bug

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
   }
   return (
     <div className="App">
-        {window.location.href == primaryURL ? <LoadingPage/> :""}
+        {window.location.href === primaryURL ? <LoadingPage/> :""}
       <Routes>
         <Route path='/' 
         element={
